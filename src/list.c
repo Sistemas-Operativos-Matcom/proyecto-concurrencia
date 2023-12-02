24d34a881b2bbe5de53deb1dff53f6aaba740a09
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2023-12-01 20:19:14 -0500

    feat: add linked list method handlers.

--- src/list.c
+++ src/list.c
@@ -1,43 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "list.h"
 
 // Init list structure
+node_t* root_node = NULL;
+
 int init_list(int_ll_t *list)
 {
-    // TODO: Your code here!
-    return 0;
+   list->root = NULL;
+   list->size = 0;
+   return 0;
+}
+
+// Size of the List
+int size_list(int_ll_t *list)
+{
+    return list->size;
 }
 
 // Free list structure
 int free_list(int_ll_t *list)
 {
-    // TODO: Your code here!
-    return 0;
-}
+   node_t* curr = list->root;
+   node_t* next;
 
-// Get list size
-int size_list(int_ll_t *list)
-{
-    // TODO: Your code here!
-    return 0;
-}
+   while (curr != NULL)
+   {
+       next = curr->next;
+       free(curr);
+       curr = next;
+   }
 
-// Get element at index
-int index_list(int_ll_t *list, int index, int *out_value)
-{
-    // TODO: Your code here!
-    return 0;
+   list->root = NULL;
+   list->size = 0;
+   return 0;
 }
 
 // Insert element at index
 int insert_list(int_ll_t *list, int index, int value)
 {
-    // TODO: Your code here!
-    return 0;
+   node_t* new_node = (node_t*)malloc(sizeof(node_t));
+   new_node->value = value; 
+   new_node->next = NULL;
+
+   node_t* curr = list->root; 
+   node_t* prev = NULL;
+
+   if(index == 0)
+   {
+       new_node->next = curr;
+       list->root = new_node;
+   }
+   else
+   {
+       for (size_t i = 0; i < index; i++)
+       {
+           prev = curr;
+           curr = curr->next;
+       }
+
+       new_node->next = curr;
+       prev->next = new_node;
+   }
+
+   list->size++;
+   return 0; 
 }
 
 // Remove element at index
 int remove_list(int_ll_t *list, int index, int *out_value)
 {
-    // TODO: Your code here!
-    return 0;
+   node_t* curr = list->root; 
+   node_t* prev = NULL;
+
+   for (size_t i = 0; i < index; i++)
+   {
+       prev = curr;
+       curr = curr->next;
+   }
+
+   prev->next = curr->next;
+   *out_value = curr->value;
+
+   free(curr);
+   list->size--;
+   return 0; 
+}
+
+// Get element at index
+int index_list(int_ll_t *list, int index, int *out_value)
+{
+   index = (index >= list->size)? list->size-1 : index;
+   index = (index < 0)? 0 : index; 
+
+   if(list->size == 0)
+       return 1;
+
+   node_t* curr = list->root; 
+   for (size_t i = 0; i < index; i++)
+   {
+       curr = curr->next;
+   }
+
+   *out_value = curr->value;
+   return 0;
 }
