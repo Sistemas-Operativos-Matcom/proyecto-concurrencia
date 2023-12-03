4bb0b2360604637757a44d8da2e1150f60b0bd2e
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2023-12-03 03:29:24 -0500

    refac: method handlers of linked list.

--- src/list.c
+++ src/list.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include "list.h"
 
-// Init list structure
-node_t* root_node = NULL;
 
+// Init the list 
 int init_list(int_ll_t *list)
 {
-   list->root = NULL;
-   list->size = 0;
-   return 0;
+    list->root = NULL;
+    list->size = 0;
+    return 0;
 }
 
 // Size of the List
@@ -21,87 +20,134 @@ int size_list(int_ll_t *list)
 // Free list structure
 int free_list(int_ll_t *list)
 {
-   node_t* curr = list->root;
-   node_t* next;
+    node_t *curr = list->root;
+    node_t *next;
 
-   while (curr != NULL)
-   {
-       next = curr->next;
-       free(curr);
-       curr = next;
-   }
+    while (curr != NULL)
+    {
+        next = curr->next;
+        free(curr);
+        curr = next;
+    }
 
-   list->root = NULL;
-   list->size = 0;
-   return 0;
+    list->root = NULL;
+    list->size = 0;
+    return 0;
 }
 
 // Insert element at index
 int insert_list(int_ll_t *list, int index, int value)
 {
-   node_t* new_node = (node_t*)malloc(sizeof(node_t));
-   new_node->value = value; 
-   new_node->next = NULL;
+    // Fixing index.
+    index = (index < 0)? 0 : index;
+    index = (index >= list->size)? list->size-1 : index;
 
-   node_t* curr = list->root; 
-   node_t* prev = NULL;
+    // Creating the new node to add.
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    new_node->value = value;
+    new_node->next = NULL;
 
-   if(index == 0)
-   {
-       new_node->next = curr;
-       list->root = new_node;
-   }
-   else
-   {
-       for (size_t i = 0; i < index; i++)
-       {
-           prev = curr;
-           curr = curr->next;
-       }
-
-       new_node->next = curr;
-       prev->next = new_node;
-   }
-
-   list->size++;
-   return 0; 
+    // Adding the new node.
+    node_t *curr = list->root;
+    if (curr == NULL)
+    {
+        list->root = new_node;
+    }
+    else 
+    {
+        for (int i = 0; i < index; i++)
+        {
+            curr = curr->next;
+        }
+        new_node->next = curr->next;
+        curr->next = new_node;
+    }
+    list->size++;  
+    return 0;
 }
+
 
 // Remove element at index
 int remove_list(int_ll_t *list, int index, int *out_value)
 {
-   node_t* curr = list->root; 
-   node_t* prev = NULL;
+    // Fixing index.
+    index = (index < 0)? 0 : index;
+    index = (index >= list->size)? list->size-1 : index;
 
-   for (size_t i = 0; i < index; i++)
-   {
-       prev = curr;
-       curr = curr->next;
-   }
+    // Finding the node.
+    node_t *curr = list->root;
 
-   prev->next = curr->next;
-   *out_value = curr->value;
-
-   free(curr);
-   list->size--;
-   return 0; 
+    if (curr == NULL)
+    {
+        return 1;
+    }
+    else 
+    {
+        node_t* prev = NULL;  
+        for (int i = 0; i < index; i++)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        // Deleting the node and fixing references.
+        *out_value = curr->value;
+        if(prev != NULL)
+            prev->next = curr->next;
+        free(curr);
+    }
+    list->size--;  
+    return 0;
 }
 
 // Get element at index
 int index_list(int_ll_t *list, int index, int *out_value)
 {
-   index = (index >= list->size)? list->size-1 : index;
-   index = (index < 0)? 0 : index; 
+    // Fixing index.
+    index = (index < 0)? 0 : index;
+    index = (index >= list->size)? list->size-1 : index;
 
-   if(list->size == 0)
-       return 1;
+    node_t *curr = list->root;
+    if(curr == NULL)
+        return 1;
+    
+    // Finding the node.
+    for (int i = 0; i < index; i++)
+    {
+        curr = curr->next;
+    }
 
-   node_t* curr = list->root; 
-   for (size_t i = 0; i < index; i++)
-   {
-       curr = curr->next;
-   }
-
-   *out_value = curr->value;
-   return 0;
+    *out_value = curr->value;
+    return 0;
 }
+
+
+// int main()
+// {
+//     int_ll_t* list = malloc(sizeof(int_ll_t));
+//     init_list(list);
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         insert_list(list,i,i);
+//     }
+
+//     int arr[10]; 
+//     int val = -1;
+//     for (int i = 0; i < 10; i++)
+//     {
+//         index_list(list,i,&val);
+//         arr[i] = val;
+
+//         // printf("val at %d ", i);
+//         // printf("%d ", val); 
+//     }
+    
+//     remove_list(list, 4 , &val);
+//     remove_list(list, 8 , &val);
+
+//     insert_list(list, 3 , 4);
+//     insert_list(list, 8 , 9);
+
+    
+//     return 0;
+// }
