38ed16503d7d34580ff6220e6a058a0f8764e961
Alfredo Nuno <anuno61044@gmail.com>
2023-12-03 00:42:21 -0500

    freelist & size_list

--- src/list.c
+++ src/list.c
@@ -3,8 +3,11 @@
 // Init list structure
 int init_list(int_ll_t *list)
 {
+    // Inicializar atributos de la lista
     list->root = NULL;
     list->size = 0;
+
+    // Inicializar lock
     pthread_mutex_init(&list->lock, NULL);
     return 0;
 }
@@ -12,15 +15,29 @@ int init_list(int_ll_t *list)
 // Free list structure
 int free_list(int_ll_t *list)
 {
-    // TODO: Your code here!
+    pthread_mutex_lock(&list->lock);
+    // Eliminar y limpiar cada nodo de la lista
+    while (list->root != NULL)
+    {
+        node* temp = list->root;
+        list->root = list->root->next;
+        free(temp);
+    }
+    list->size = 0;
+
+    // Destruir los locks
+    pthread_mutex_unlock(&list->lock);
+    pthread_mutex_destroy(&list->lock);
     return 0;
 }
 
 // Get list size
 int size_list(int_ll_t *list)
 {
-    // TODO: Your code here!
-    return 0;
+    pthread_mutex_lock(&list->lock);
+    int size = list->size;
+    pthread_mutex_unlock(&list->lock);
+    return size;
 }
 
 // Get element at index
