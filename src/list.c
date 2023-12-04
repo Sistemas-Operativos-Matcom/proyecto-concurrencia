2a7ef59ed6d406f75ced077d07baf594272bbddd
Carlos <carlos@Ubuntu.myguest.virtualbox.org>
2023-12-04 01:35:33 -0500

    terminado

--- src/list.c
+++ src/list.c
@@ -1,43 +1,153 @@
 #include "list.h"
 
 // Init list structure
-int init_list(int_ll_t *list)
-{
-    // TODO: Your code here!
+int init_list(linkedlist_t *list){
+    list->root = NULL;
+    list->size = 0;
+
+    pthread_mutex_init(&list->lock, NULL);
     return 0;
 }
 
 // Free list structure
-int free_list(int_ll_t *list)
-{
-    // TODO: Your code here!
+int free_list(linkedlist_t *list){
+    pthread_mutex_lock(&list->lock);
+    while (list->root != NULL)
+    {
+        node* temp = list->root;
+        list->root = list->root->next;
+        free(temp);
+    }
+    list->size = 0;
+
+    pthread_mutex_unlock(&list->lock);
+    pthread_mutex_destroy(&list->lock);
     return 0;
 }
 
 // Get list size
-int size_list(int_ll_t *list)
-{
-    // TODO: Your code here!
-    return 0;
+int size_list(linkedlist_t *list){
+    pthread_mutex_lock(&list->lock);
+    int size = list->size;
+    pthread_mutex_unlock(&list->lock);
+    return size;
 }
 
 // Get element at index
-int index_list(int_ll_t *list, int index, int *out_value)
-{
-    // TODO: Your code here!
+int index_list(linkedlist_t *list, int index, int *out_value){
+    pthread_mutex_lock(&list->lock);
+    
+    if (list->size == 0)
+    {
+        pthread_mutex_unlock(&list->lock);
+        return 1;
+    }
+
+    if (index < 0)
+    {
+        index = 0;
+    }
+    if (index >= list->size)
+    {
+        index = list->size-1;
+    }
+
+    int count = 0;
+    node* temp = list->root;
+    while (count < index && temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+
+    if (temp != NULL) {
+        *out_value = temp->value;
+    }
+    pthread_mutex_unlock(&list->lock);
     return 0;
 }
 
 // Insert element at index
-int insert_list(int_ll_t *list, int index, int value)
-{
-    // TODO: Your code here!
+int insert_list(linkedlist_t *list, int index, int value){
+    pthread_mutex_lock(&list->lock);
+
+    if (index < 0)
+    {
+        index = 0;
+    }
+    if (index > list->size)
+    {
+        index = list->size;
+    }
+    
+    if (index == 0)
+    {
+        node* new = (node*)malloc(sizeof(node));
+        new->value = value;
+
+        new->next = list->root;
+        list->root = new;
+        list->size++;
+        pthread_mutex_unlock(&list->lock);
+        return 0;
+    }
+
+    int count = 0;
+    node* temp = list->root;
+    while (count < index - 1)
+    {
+        count++;
+        temp = temp->next;
+    }
+
+    node* new = (node*)malloc(sizeof(node));
+    new->value = value;
+
+    new->next = temp->next;
+    temp->next = new;
+    list->size++;
+    pthread_mutex_unlock(&list->lock);
     return 0;
 }
 
 // Remove element at index
-int remove_list(int_ll_t *list, int index, int *out_value)
-{
-    // TODO: Your code here!
+int remove_list(linkedlist_t *list, int index, int *out_value){
+    pthread_mutex_lock(&list->lock);
+    if (list->size == 0)
+    {
+        pthread_mutex_unlock(&list->lock);
+        return 1;
+    }
+    
+    if (index <= 0)
+    {
+        node* temp = list->root;
+        *out_value = temp->value;
+        list->root = list->root->next;
+        free(temp);
+        list->size--;
+        pthread_mutex_unlock(&list->lock);
+        return 0;
+    }
+    
+    if (index >= list->size)
+    {
+        index = list->size -1;
+    }
+
+    int count = 0;
+    node* temp = list->root;
+    while (count < index-1)
+    {
+        count++;
+        temp = temp->next;
+    }
+
+    node* next = temp->next;
+    *out_value = next->value;
+    temp->next = temp->next->next;
+    free(next);
+    list->size--;
+    pthread_mutex_unlock(&list->lock);
     return 0;
 }
--- src/list.h
+++ src/list.h
@@ -1,26 +1,37 @@
 #ifndef INT_LL_H
 #define INT_LL_H
-// Integer Linked Lists
-typedef struct _integer_linked_list_t
+
+#include <pthread.h>
+#include <stdlib.h>
+
+typedef struct _int_list_t
 {
-    // TODO: Your code here!
-} int_ll_t;
+    node* root;
+    int size;
+    pthread_mutex_t lock;
+} linkedlist_t;
+
+typedef struct node {
+    int value;
+    struct node* next;
+} node;
+
 
 // Init list structure
-int init_list(int_ll_t *list);
+int init_list(linkedlist_t *list);
 
 // Free list structure
-int free_list(int_ll_t *list);
+int free_list(linkedlist_t *list);
 
 // Get list size
-int size_list(int_ll_t *list);
+int size_list(linkedlist_t *list);
 
 // Get element at index
-int index_list(int_ll_t *list, int index, int *out_value);
+int index_list(linkedlist_t *list, int index, int *out_value);
 
 // Insert element at index
-int insert_list(int_ll_t *list, int index, int value);
+int insert_list(linkedlist_t *list, int index, int value);
 
 // Remove element at index
-int remove_list(int_ll_t *list, int index, int *out_value);
+int remove_list(linkedlist_t *list, int index, int *out_value);
 #endif
