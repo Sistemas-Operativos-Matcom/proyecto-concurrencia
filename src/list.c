ba7b858c88fc27feafdcce902d7ec8eb3ce6365d
Amanda <amandacordero.1709@gmail.com>
2023-12-05 00:37:00 -0500

    comentado

--- src/list.c
+++ src/list.c
@@ -4,8 +4,11 @@
 
 int init_list(int_ll_t *list)
 {
+    // Inicializar una lista vacia
     list->size = 0;
     list->First = NULL;
+
+    // Inicializar el bloqueo para esa lista
     pthread_mutex_init(&list->lock, NULL);
     return 0;
 }
@@ -14,6 +17,8 @@ int init_list(int_ll_t *list)
 int free_list(int_ll_t *list)
 {
     pthread_mutex_lock(&list->lock);
+    // itera por cada elemento de la lista 
+    // libera el espacio correspondiente al primer elemento de la lista
     while (list->size != 0)
     {
         Element_t *temp = list->First;
@@ -21,6 +26,7 @@ int free_list(int_ll_t *list)
         list->size--;
         free(temp);
     }
+    // devuelve la lista a 0
     list->size = 0;
     pthread_mutex_unlock(&list->lock);  
     pthread_mutex_destroy(&list->lock);
@@ -38,6 +44,7 @@ int index_list(int_ll_t *list, int index, int *out_value)
 {
     pthread_mutex_lock(&list->lock);
     if (list->size == 0)
+    // si la lista es vacia
     {
         pthread_mutex_unlock(&list->lock);
         return 1;
@@ -45,6 +52,7 @@ int index_list(int_ll_t *list, int index, int *out_value)
 
 
     if (index <= 0)
+    // si es el primer elemento
     {
         *out_value = list->First->value;
         pthread_mutex_unlock(&list->lock);
@@ -57,6 +65,7 @@ int index_list(int_ll_t *list, int index, int *out_value)
     int count = 0;
     Element_t *curr = list->First;
     while (count < index)
+    // itera hasta encontrar el indice deseado
     {
         curr = curr->next;
         count ++;
@@ -72,6 +81,7 @@ int insert_list(int_ll_t *list, int index, int value)
     
     pthread_mutex_lock(&list->lock);
     if (list->size == 0)
+    // si la lista era vacia el elemento a añadir es el cabezal de la lista
     {
         Element_t *new = malloc(sizeof(Element_t));
         new->value = value;
@@ -82,6 +92,7 @@ int insert_list(int_ll_t *list, int index, int value)
     }
 
     if (index <= 0)
+    // añadir en la primera posicion
     {
         Element_t *new = malloc(sizeof(Element_t));
         new->value = value;
@@ -98,12 +109,17 @@ int insert_list(int_ll_t *list, int index, int value)
     }
     int count = 0;
     while (count < index -1)
+    // itera hasta q curr es el nodo anterior al que se desea añadir
     {
         curr = curr->next;
         count ++;
     }
     Element_t *temp = curr->next;
     Element_t *new = malloc(sizeof(Element_t));
+
+    // el siguiente a curr es el nuevo nodo y el siguiente a este es el que 
+    // anteriormente era el siguiente a curr
+
     curr->next = new;
     new->value = value;
     new->next = temp;
@@ -117,11 +133,13 @@ int remove_list(int_ll_t *list, int index, int *out_value)
 {
     pthread_mutex_lock(&list->lock);
     if (list->size == 0)
+    // si la lista era vacia no tiene sentido eliminar
     {
         pthread_mutex_unlock(&list->lock);
         return 1;
     }
     if (list->size == 1)
+    // si la lista solo contenia un elemento este es el que se ha de eliminar
     {
         *out_value = list->First->value;
         list->size = 0;
@@ -132,6 +150,7 @@ int remove_list(int_ll_t *list, int index, int *out_value)
         return 0;
     }
     if (index <= 0)
+    // si el elemento a eliminar es el primero
     {
         *out_value = list->First->value;
         list->size = list->size -1;
@@ -148,12 +167,16 @@ int remove_list(int_ll_t *list, int index, int *out_value)
     }
     int count = 0;
     while (count < index -1)
+    // itera hasta que curr es el elemento anterior al que debe ser eliminado
     {
         curr = curr->next;
         count ++;
     }
     Element_t *temp = curr->next;
     *out_value = temp->value;
+
+    // el siguiente a curr sera el que era el siguiente del elemento a eliminar
+
     curr->next = temp->next;
     list->size --;
     free(temp);
--- src/list.h
+++ src/list.h
@@ -2,17 +2,18 @@
 #define INT_LL_H
 #include <pthread.h>
 typedef struct Element
+// estructura de los elementos de la linked list
 {
-    int value;
-    struct Element *next;
+    int value; //valor del elemento
+    struct Element *next; //puntero al siguiente elemento
 }Element_t;
 
 // Integer Linked Lists
 typedef struct _integer_linked_list_t
 {
-    int size;
-    Element_t *First;
-    pthread_mutex_t lock;
+    int size; //cantidad de elementos almacenados
+    Element_t *First; //puntero al cabezal de la lista
+    pthread_mutex_t lock; //estructura de bloqueo
 } int_ll_t;
 
 // Init list structure
